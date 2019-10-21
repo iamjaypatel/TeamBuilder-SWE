ba96a718ef1202b2c4f069963033118d569b7b90
Jay Patel <jay26597@gmail.com>
2019-10-21 18:05:14 -0400

    format

--- schema.sql
+++ schema.sql
@@ -5,18 +5,22 @@ DROP SCHEMA IF EXISTS team_builder CASCADE;
 CREATE SCHEMA team_builder;
 
 -- Create Seperate tables
-CREATE TABLE team_builder.User (
-    userID SERIAL NOT NULL,
-    username VARCHAR(20),
-    pass VARCHAR(20),
+CREATE TABLE team_builder.User
+(
+    userID     SERIAL NOT NULL,
+    username   VARCHAR(20),
+    pass       VARCHAR(20),
     first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    phone_no VARCHAR(20)
+    last_name  VARCHAR(20),
+    phone_no   VARCHAR(20)
 );
 
-CREATE TABLE team_builder.Projects ();
+CREATE TABLE team_builder.Projects
+(
+);
 
-CREATE TABLE team_builder.Project_Involved (
+CREATE TABLE team_builder.Project_Involved
+(
     uID SERIAL
 );
 
