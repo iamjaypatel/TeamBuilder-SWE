b120e82689e242998a023b92e450aff8b7fe43b9
Jay Patel <jay26597@gmail.com>
2019-10-21 18:00:52 -0400

    upload schema

---
+++ schema.sql
@@ -0,0 +1,22 @@
+-- Drop Schema if Already Exists
+DROP SCHEMA IF EXISTS team_builder CASCADE;
+
+-- Create Schema
+CREATE SCHEMA team_builder;
+
+-- Create Seperate tables
+CREATE TABLE team_builder.User (
+    userID SERIAL NOT NULL,
+    username VARCHAR(20),
+    pass VARCHAR(20),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    phone_no VARCHAR(20)
+);
+
+CREATE TABLE team_builder.Projects ();
+
+CREATE TABLE team_builder.Project_Involved (
+    uID SERIAL
+);
+
