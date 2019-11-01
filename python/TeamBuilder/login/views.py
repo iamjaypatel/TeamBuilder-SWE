3e4df8b69d26cc28804a217076d038400567e3e7
zbird100 <39056153+zbird100@users.noreply.github.com>
2019-11-01 15:11:58 -0400

    started creating views for login and extending templates

--- python/TeamBuilder/login/views.py
+++ python/TeamBuilder/login/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world, this is the login page")
+    return render(request,'login/index.html')
 
 
