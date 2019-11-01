c689344b3a95c9924ebaa7ae144168cc522e82e4
zbird100 <39056153+zbird100@users.noreply.github.com>
2019-11-01 15:57:43 -0400

    added models and superuser

--- python/TeamBuilder/login/models.py
+++ python/TeamBuilder/login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+#    profileID = models.CharField( primary_key=True)
+   userName = models.CharField(max_length = 20)
+   userPassword = models.CharField(max_length = 60)
+# class Projects(models.Model):
+#    pass
+# class Project_Involved(models.Model):
+#    pass
