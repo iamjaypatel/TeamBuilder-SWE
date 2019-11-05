6edef9c485db3ddf48081bd647b772b4b81ef147
Samuel Talenti <Sct26@pitt.edu>
2019-11-05 15:30:50 -0500

    Unique Project outline
    Included outline for unique project page, not functional nor dynamically loaded. dummy data passed into page, "projects/11111/" is the only functional page

--- python/TeamBuilder/projects/urls.py
+++ python/TeamBuilder/projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='projects-home'),
+    path('11111/', views.unique, name='project'),
 ]
--- python/TeamBuilder/projects/views.py
+++ python/TeamBuilder/projects/views.py
@@ -28,3 +28,18 @@ def home(request):
         'posts': posts
     }
     return render(request,'projects/projhome.html', data)
+
+def unique(request):
+    posts = {
+        'projectID':'11111',
+        'projectAdmin':'Sam',
+        'projectName': 'Deliverable5',
+        'descr': 'Insert description here',
+        'maxCap': '10',
+        'spaceTaken': '1',
+        'spaceAvailable': '9' 
+    }
+    data = {
+        'posts': posts
+    }
+    return render(request,'projects/project.html', data)
