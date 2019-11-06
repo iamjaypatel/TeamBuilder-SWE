c17ab1411695e6897e3b8e2874f75a34744c49b8
zbird100 <39056153+zbird100@users.noreply.github.com>
2019-11-06 16:30:32 -0500

    started on dynamic view for profile

--- python/TeamBuilder/login/views.py
+++ python/TeamBuilder/login/views.py
@@ -5,4 +5,12 @@ from django.shortcuts import render
 def index(request):
     return render(request,'login/index.html')
 
-
+# def profile(request,username):
+#     info = {
+#         'username':username,
+#         'email':'test@temp.com',
+#     }
+#     data ={
+#         'info':info
+#     }
+#     return render(request,'login/profile.html',info)
