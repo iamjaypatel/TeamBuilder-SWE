de7a96dbf2e510cf3edbe16803cb9fa8937ffc2c
zbird100 <39056153+zbird100@users.noreply.github.com>
2019-11-11 18:02:37 -0500

    updating styling for projects

--- python/TeamBuilder/projects/urls.py
+++ python/TeamBuilder/projects/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='projects-home'),
-    path('11111/', views.unique, name='project'),
-    path('<int:project_id>/',views.uniqueP, name ='uProject'),
+    path('<int:project_id>/',views.uniqueP, name ='project'),
 ]
--- python/TeamBuilder/projects/views.py
+++ python/TeamBuilder/projects/views.py
@@ -29,20 +29,6 @@ def home(request):
     }
     return render(request,'projects/projhome.html', data)
 
-def unique(request):
-    posts = {
-        'projectID':'11111',
-        'projectAdmin':'Sam',
-        'projectName': 'Deliverable5',
-        'descr': 'Insert description here',
-        'maxCap': '10',
-        'spaceTaken': '1',
-        'spaceAvailable': '9' 
-    }
-    data = {
-        'posts': posts
-    }
-    return render(request,'projects/project.html', data)
 
 def uniqueP(request, project_id):
     # we will be doing same as unique but instead will query for the project
