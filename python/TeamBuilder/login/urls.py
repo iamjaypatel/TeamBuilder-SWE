02229a32dd7ca1cb2f3fd46b684262bcfc8ac316
zbird100 <39056153+zbird100@users.noreply.github.com>
2019-11-19 16:59:35 -0500

    Update urls.py

--- python/TeamBuilder/login/urls.py
+++ python/TeamBuilder/login/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='login-user'),
     path('u/<str:username>/', views.profile , name='user-page'),
-    path('mypage/', views.myprof, name='my-page'),
     path('logout/', views.logout_view, name='logout'),
     path("register/", views.register, name="register"),
 ]
