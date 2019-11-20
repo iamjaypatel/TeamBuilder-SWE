7debe1a6b7ec451424cfd24e533facb859d1ad27
Samuel Talenti <Sct26@pitt.edu>
2019-11-20 13:28:09 -0500

    Join, Leave, Accept, Reject, and Delete functionality for project page

--- python/TeamBuilder/projects/urls.py
+++ python/TeamBuilder/projects/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.home, name='projects-home'),
     path('<int:ID>/',views.uniqueP, name ='uProject'),
     path('<int:ID>/join/', views.joinP, name ='jProject'),
+    path('<int:ID>/leave/', views.leaveP, name='lProject'),
+    path('<int:ID>/delete/', views.deleteP, name='dProject'),
+    path('<int:ID>/a/<str:username>', views.acceptP, name='aProject'),
+    path('<int:ID>/r/<str:username>', views.rejectP, name='rProject'),
     path('createProj/', views.createProjView, name='createP'),
     path('myProjects/', views.myProjectsView, name='projects-my'),
 ]
